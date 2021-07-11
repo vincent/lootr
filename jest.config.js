10dc2c84338214ce99f6978e72bc018b771a013a
Zeno <airtonix@users.noreply.github.com>
2021-07-11 18:06:12 +0930

    Fix/update documentation (#2)
    * convert
    
    * fix tests
    
    * update docs, create reports
    
    * add junit

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,25 @@ module.exports = {
   setupFilesAfterEnv: ['./jest.setup.ts'],
   testEnvironment: 'node',
   moduleFileExtensions: ['js', 'ts'],
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: 'reports',
+        outputName: 'jest-junit.xml',
+        ancestorSeparator: ' › ',
+        uniqueOutputName: 'false',
+        suiteNameTemplate: '{filepath}',
+        classNameTemplate: '{classname}',
+        titleTemplate: '{title}',
+      },
+    ],
+  ],
   verbose: true,
   globals: {
     'ts-jest': {
-      tsconfig: "./tsconfig.test.json",
+      tsconfig: './tsconfig.test.json',
       diagnostics: { pretty: true },
       isolatedModules: false,
     },
